996d116c3b53f1b7f8b984c8ddbc5c53c8b6b1c2
Dustin McCraw <dustin.mccraw@gatsbyjs.com>
2020-12-18 09:53:10 -0800

    remove client side routes

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-netlify`,
-    {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/*`] },
-    },
     `gatsby-plugin-gatsby-cloud`,
     {
       resolve: 'gatsby-plugin-web-font-loader',
